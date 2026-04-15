0c734654cab08634c2ea94677943896737c64bdc
Wkhortencia2 <wkdoph2@gmail.com>
2026-04-15 17:09:11 -0300

    Add files via upload

---
+++ ex01.js
@@ -0,0 +1,32 @@
+//criar uma variavel idade e mostrar:
+//"eu tenho _____ anos de idade"
+let idade = 16;
+console.log(`eu tenho ${16} anos de idade`);
+//console.log('eu tenho' + idade + 'anos de idade');
+
+//criar duas variaveis 'cidade' e 'estado' e exiba:
+//eu moro em ____ - ______.
+let cidade = 'maringa';
+let estado = 'parana';
+console.log(`Eu moro em ${cidade} - ${estado}`);
+
+//criar duas variaveis e mostre:
+//a soma, a subtracao, a multiplicação, a divisao, o dobro, o triplo, a metade e o quadrado
+let n1 = 10;
+let n2 = 5;
+let soma = n1 + n2;
+console.log(`A soma de ${n1} e ${n2} é ${soma}`);
+let subt = n1 - n2;
+console.log(`A subt de ${n1} e ${n2} é ${subt}`);
+let mult = n1 * n2;
+console.log(`A mult de ${n1} e ${n2} é ${mult}`);
+let divisao = n1 / n2;
+console.log(`A divisao de ${n1} e ${n2} é ${divisao}`);
+let dobro = n1 * 2;
+console.log(`A dobro de ${n1} e ${n2} é ${dobro}`);
+let tripulo = n2 * 3;
+console.log(`A tripulo de ${n1} e ${n2} é ${tripulo}`);
+let metade = n1 / 2;
+console.log(`A metade de ${n1} e ${n2} é ${metade}`);
+let quadrado = n1 ** 2;
+console.log(`A quadrado de ${n1} e ${n2} é ${quadrado}`);
---
+++ ex03.js
@@ -0,0 +1,13 @@
+let nome = 'Werik';
+console.log(nome);
+
+let cidade = 'Maringá';
+let ano =  2011;
+console.log(`Eu moro em ${cidade} e nasci no ano de ${ano}`);
+
+let idade = 16;
+if (idade >= 18) {
+    console.log('Pode dirigir')
+} else {
+    console.log('Não pode')
+}
